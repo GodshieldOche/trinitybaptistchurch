811ff28df5418d9b2aaa58d512a0af6820634d10
Godshield Oche Godshield <97735610+GodshieldOche@users.noreply.github.com>
2022-04-08 19:03:05 +0100

    menu almost done

--- components/Menu.js
+++ components/Menu.js
@@ -8,18 +8,17 @@ const Menu = ({ menuState }) => {
     const dispatch = useDispatch()
 
     return (
-        <div
-            className={`${menuState ? " w-full md:w-[40%] " : "w-0"} lg:hidden py-5 fixed transition-all duration-700 ease-in-out bg-primary-black h-screen top-0 right-0 !z-50 !overflow-x-hidden `}>
+        <div className={`${menuState ? " w-full md:w-[40%] " : "w-0"} lg:hidden py-5 fixed transition-all duration-700 ease-in-out bg-[white] h-screen top-0 right-0 !z-50 !overflow-x-hidden `}>
             <div className="flex flex-col w-full px-5">
                 <div className="flex justify-end">
-                    <CloseIcon className={`  ${menuState ? "text-[white]/80 " : "text-primary-black"} cursor-pointer transition-all duration-500 ease-in-out font-light`}
+                    <CloseIcon className={`  ${menuState ? " text-primary-black " : "text-[white]/80"} cursor-pointer transition-all duration-500 ease-in-out font-light`}
                         onClick={() => {
                             dispatch(setMenuState(false))
                         }}
                     />
                 </div>
               
-                <div className={`flex flex-col px-3 py-3 space-y-5 `}>
+                <div className={`flex flex-col px-3 py-3 space-y-7  `}>
                     <MenuLinks title={"ministries"} />
                     <MenuLinks title={"about"} />
                     <MenuLinks title={"resources"} />
--- components/MenuLinks.js
+++ components/MenuLinks.js
@@ -3,25 +3,67 @@ import { useRouter } from "next/router"
 import { useSelector } from "react-redux"
 import { setMenuState } from "../redux/features/menu"
 import { useDispatch } from "react-redux"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useState } from "react"
 
 const MenuLinks = ({ title }) => {
+    const [ministries, setMinistries] = useState(false)
     const router = useRouter()
     const { menuState } = useSelector(state => state.menu)
     const dispatch = useDispatch()
+    
+    const handleClick = () => {
+        router.push(`/${title}`)
+        dispatch(setMenuState(false))
+     }
+
+     
 
     return (
-        <Link href={`/${title}`} className="">
-            <a >
-                <h1 className={`
-                ${menuState ? "text-[white]/70 " : "!text-primary-black"}
-                ${router.pathname.split("/")[1] === title ? "font-medium !text-[white]" : "font-light"}
-                text-xl md:text-lg hover:font-medium hover:!text-[white]/60 capitalize`}
-                    onClick={() => {
-                        dispatch(setMenuState(false))
-                    }}
-                >{title}</h1>
-            </a>
-        </Link>
+        <div className="flex flex-col space-y-3 mt-4 ">
+            <div className={`flex w-full justify-between items-center ${title === "ministries" ? "space-x-1" : "space-x-0"} ${menuState ? "!text-primary-black" : "text-[white]"} 
+            transition-all duration-300 ease-in-out`}
+                onClick={() => {
+                    if (title === "ministries") {
+                        setMinistries(!ministries)
+                        console.log(ministries)
+                    } else {
+                        handleClick()
+                    }
+                }}
+            >
+
+                <h1 className={`${router.pathname.split("/")[1] === title ? "font-medium" : "font-light"} text-2xl md:text-xl md:hover:font-medium md:hover:!text-[white]/60 capitalize cursor-pointer`}>
+                    {title}
+                </h1>
+                
+                {
+                    title === "ministries" &&
+                    <div>
+                        {
+                            ministries ?
+                                <KeyboardArrowUpIcon className=" text-xl " />
+                                :
+                                <KeyboardArrowDownIcon className=" text-xl " />
+                                
+                        }
+
+                    </div>
+                }
+
+            </div>
+
+            {
+                title === "ministries" && ministries && menuState &&
+                <div className={`flex flex-col w-full font-light space-y-1`}>
+                    <a>Men</a>
+                    <a>Women</a>
+                    <a>Children</a>
+                </div>
+            }
+        </div>
+        
     )
 }
 
