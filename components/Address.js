2e81692843f7c1964065eb514f2106173efb7031
Godshield Oche Godshield <97735610+GodshieldOche@users.noreply.github.com>
2022-04-18 09:47:09 +0100

    touches

--- components/Address.js
+++ components/Address.js
@@ -6,7 +6,7 @@ const Address = () => {
             <div className="">
                 <div className="w-full rounded-2xl h-[500px] relative">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.811014338845!2d7.431193214786354!3d8.98953089354829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b4579802d1f%3A0x95fb206cd5ea74f5!2sTrinity%20Baptist%20Church%2C%20Abuja!5e0!3m2!1sen!2sng!4v1650270096555!5m2!1sen!2sng"
-                        className="absolute top-0 left-0 bottom-0 right-0 w-full h-full" allowFullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        className="absolute top-0 left-0 bottom-0 right-0 w-full h-full" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className="flex sm:hidden flex-col px-2 py-5 w-full space-y-1 bg-[white]">
                     <h1 className="text-primary-dark font-medium  uppercase text-lg !mb-3">Address</h1>
