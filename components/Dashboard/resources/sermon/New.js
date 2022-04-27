250f1ad56ef62c6ff7fbcc376f5618ef1afd25aa
Godshield Oche Godshield <97735610+GodshieldOche@users.noreply.github.com>
2022-04-27 16:29:33 +0100

    new

--- components/Dashboard/resources/sermon/New.js
+++ components/Dashboard/resources/sermon/New.js
@@ -196,7 +196,7 @@ const New = () => {
                             {/* image upload */}
                             <div className="space-y-3 w-full">
                                 <h1 className=" uppercase text-sm">Image</h1>
-                                <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-44 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-44 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                         <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 capitalize">Click to select</p>
                                         <p className="text-xs text-gray-500 dark:text-gray-400">PNG or JPG </p>
