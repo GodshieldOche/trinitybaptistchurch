253a626ae8e83b36b44cf6aabb01b26275b3849f
Godshield Oche Godshield <97735610+GodshieldOche@users.noreply.github.com>
2022-05-16 17:55:16 +0100

    client biblestudy done

--- components/common/About.js
+++ components/common/About.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const About = ({ preacher }) => {
   return (
-      <div className="flex flex-col w-full rounded-2xl items-center text-gray-200 justify-center bg-gray-700 space-y-5 px-2 py-5 md:p-5">
+      <div className="flex flex-col w-full rounded-2xl items-center text-gray-200 justify-center bg-gray-700 space-y-5 px-4 py-5 md:p-5">
           <h1 className="text-sm text-gray-100 lg:text-base font-medium uppercase">About the speaker</h1>
           <div className="w-[80px] h-[80px] rounded-full float-left mr-2  relative ">
               <Image src={preacher?.imageUrl?.url}
