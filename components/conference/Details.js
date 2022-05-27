e5b7b95cdd903ca1321016a69f9b6020e0d3e676
Godshield Oche Godshield <97735610+GodshieldOche@users.noreply.github.com>
2022-05-27 23:33:08 +0100

    details page

--- components/conference/Details.js
+++ components/conference/Details.js
@@ -42,7 +42,7 @@ const Details = () => {
     };
 
     return (
-        <div className={` !mb-20 w-full space-y-2`}>
+        <div className={` !mb-20 w-full`}>
             <Player resource={current} />
             <Body series={conference} scrollToTop={scrollToTop} scrollToAll={scrollToAll}
                 current={current} changeCurrent={changeCurrent} />
