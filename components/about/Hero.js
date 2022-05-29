2b9376863496454860973ea0f071958765c6c49c
Godshield Oche Godshield <97735610+GodshieldOche@users.noreply.github.com>
2022-05-29 07:17:56 +0100

    fixed

--- components/about/Hero.js
+++ components/about/Hero.js
@@ -3,7 +3,7 @@ const Hero = () => {
         <div className="bg-gradient-to-r from-primary-dark to-primary-light text-[white] pt-[100px]  md:pt-[68px] ">
             <div className="container px-2 md:px-0 lg:px-[2rem] pb-16 pt-10 md:pb-36 md:pt-28  flex flex-col items-center justify-center space-y-5">
                 <h1 className="text-xl md:text-3xl text-center uppercase font-medium">About TBCA</h1>
-                <h2 className="text-sm md:text-base px-2 capitalize max-w-lg mx-auto font-light text-center">
+                <h2 className="text-sm md:text-base capitalize max-w-lg mx-auto font-light text-center">
                     God-focused, christ-centered, gospel-driven church located at the heart if abuja, aimed at being a people zealous for God's holiness and
                     glory in a joyful and loving society
                 </h2>
